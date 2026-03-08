87b3c7f37607854c1165da14f8093eac6be58120
Neha Sujith <neha.sq8@gmail.com>
2026-03-08 00:11:19 -0600

    lab 7 init

---
+++ Lab_7/lab7.py
@@ -0,0 +1,21 @@
+import arcpy
+
+source = r"C:\Users\neha2003\Desktop\sujith-online-GEOG676-spring2026\Lab_7"
+band1 = arcpy.sa.Raster(source + r"\landsat4\blue.tif") #blue
+band2 = arcpy.sa.Raster(source + r"\landsat4\green.tif") #green
+band3 = arcpy.sa.Raster(source + r"\landsat4\red.tif") #red
+band4 = arcpy.sa.Raster(source + r"\landsat4\nir08.tif") #NIR
+combined = arcpy.CompositeBands_management([band1, band2, band3, band4], source + r"\output_combined.tif")
+
+# Hillshade
+azimuth = 315
+altitude = 45
+shadows = 'NO_SHADOWS'
+z_factor = 1
+arcpy.ddd.HillShade(source + r"\dem\DEM.tif", source + r"\output_Hillshade.tif", azimuth, altitude, shadows, z_factor)
+
+#Slope
+output_measurement = "DEGREE"
+z_factor = 1
+arcpy.ddd.Slope(source + "\DEM.tif", source + r"\output_Slop.tif", output_measurement, z_factor)
+print("success!")
